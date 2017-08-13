a62b26a6a828d2bf00e5055362b1b9a34380b4ac
Prashant <prashantgoldi@gmail.com>
2017-08-13 23:52:57 +0530

    adding JavaScript.js file

---
+++ JavaScript.js
@@ -0,0 +1,15 @@
+//Declaring variable
+
+var myString = 'Prashant';
+var myNumber = 7
+var myBoolean = true
+
+console.log('My name is ' + myString , 'and i like eating ' + myNumber + ' times. It\'s ' + myBoolean)
+
+console.log(365/27) // division
+console.log(365%27) // modulus
+
+// Math.random
+var myRandomNumber = Math.random()
+console.log(myRandomNumber * 100)
+console.log(Math.floor(myRandomNumber * 100))
