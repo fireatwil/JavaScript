ccdeff41198fa1afe8181d961a037a292a025368
Prashant <prashantgoldi@gmail.com>
2017-08-14 14:23:04 +0530

    modifying JavaScript.js file with brief concept of linking JavaScript to HTML and CSS with jQuery

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,6 @@
 /*
 This document is written by Prashant
-Overview of JavaScript from
-https://www.codecademy.com/courses/learn-javascript
+
 */
 
 //Declaring variable
@@ -162,3 +161,130 @@ while (currentCard != expectedCard) {
   currentCard = cards[randomNumber];
 }
 console.log("Finally, found " + currentCard);
+
+/*
+LINKING JAVASCRIPT to HTML and CSS
+
+Summary:
+How to link a JavaScript file to an HTML file using a <script> tag.
+jQuery is a library to help JavaScript interact with HTML elements.
+We can make sure our page is ready to go with $(document).ready(). Then, we can pass in a function to ready that will execute when the page is loaded.
+jQuery uses the same selector names as CSS.
+We can hide elements with hide, and show them with show.
+We can make elements appear with fadeIn.
+on('click') functions allow us to make HTML elements clickable. When an element is clicked, the click function will execute the function we provide. It's full sytnax looks like:
+$('.example-class').on('click', function() {
+  // Execute when .example-class is clicked
+});
+toggle will toggle an element on and off the page.
+$(this) will select the specific element that was clicked if placed inside a click function.
+toggleClass can toggle a class on and off.
+We can select elements next to each other with next.
+text will replace a DOM element's text with text we specify.
+slideToggle will make an element slide into and out of the page with an animation.
+
+
+// Practice
+
+// link javaScript to index.html
+<script src='/JavaScript.js'></script>
+
+DOM - Document Object Model
+  term for an elements in an HTML file
+
+// select HTML elements
+var skillset = document.getElementsByClassName('skillset');
+alert(skillset)
+
+JQUERY
+<script src='https://code.jquery.com/jquery-3.1.0.js'></script>
+function main() {   // main function in JavaScript.js file which is linked to HTML
+var $skillset = $('.skillset');
+alert($skillset);
+
+// to hide the elements
+$('.skillset').hide(); //The hide function will add the CSS property display: none to the DOM element from the page, which will hide it.
+
+// fadeIn
+$('.skillset').fadeIn(1000); // We must start with an element that is not currently displayed on the page.
+
+// show
+$('.skillset').show(); //show will change the CSS attribute display: none to a visible display property, therefore showing the element.
+
+// on click
+// .on('click', function() { ... }) adds a click listener to the selector.
+// When it's clicked the function will execute the code within its block.
+$('.projects').hide();
+$('.projects-button').on('click', function(){ //
+  $('.projects').show();
+})
+
+// toggle - When toggle is executed, it will hide or show the element that the selector points to.
+// If the element is currently hidden, toggle will show the element, and vice versa.
+$('.projects-button').on('click', function() {
+    $('.projects').toggle();
+})
+
+// toggleClass('active')
+// .toggleClass is a function that will toggle a CSS class on the jQuery selector it's connected to.
+// If the element has the class applied to it, toggleClass will remove it, and if the element does not have the class, it will add it.
+// 'active' is the class that we will toggle on and off. Notice that toggleClass does not require us to include the period before 'active' since it's already expecting a CSS class.
+$('.projects-button').on('click', function() {
+  $('.projects-button').toggleClass('active');
+})
+
+
+// this
+// $(this) selects the clicked element. If there are multiple elements with a class of .example-class, this will only toggle the class of the one that is clicked on.
+// Notice that $(this) does not require quotes around it, since it is not a CSS class. Instead, this is a JavaScript keyword.
+// $(this) behaves just like our other selectors. We can attach toggleClass or toggle to it in the same way.
+$('.projects-button').on('click', function() {
+    $(this).toggleClass('active');
+})
+
+
+// next()
+// next() can help us select elements that are next to another element.
+$('.projects-button').on('click', function() {
+    $(this).next().hide();
+})
+
+// text('some text inside ')
+// Inside of text's parentheses, we can provide text that will become the text of our DOM element.
+// The text we supply will replace any existing text, and if the element has no pre-existing text, text will add it.
+$('.projects-button').on('click', function() {
+    $(this).text('Projects Viewed');
+})
+
+// slideToggle(1000) // 1000 in ms
+// slideToggle also takes a parameter of milliseconds that the animation should last. The default is 400 milliseconds, or 0.4 seconds.
+$('.projects-button').on('click', function() {
+  $(this).next().slideToggle(400);
+});
+
+}
+$(document).ready(main);
+
+main here is a callback, which means that our code will wait until the document
+(in other word, the DOM) is loaded, or ready.
+When it is, then it will execute the main function.
+
+For all above examples, refer the below sample code
+
+function main() {
+  $('.skillset').hide();
+  $('.skillset').fadeIn(1000);
+
+  $('.projects').hide();
+
+  $('.projects-button').on('click', function() {
+		$(this).next().toggle();
+  //  $(this).next().slideToggle(400);
+    $(this).toggleClass('active');
+    $(this).text('Projects Viewed');
+	});
+}
+
+$(document).ready(main);
+
+*/
